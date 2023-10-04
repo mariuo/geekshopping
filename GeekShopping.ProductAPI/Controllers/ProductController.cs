f9431093aee1a91f2c97abf0b36084cd720a7d88
Mario Camelo <mariuo@gmail.com>
2023-10-03 20:24:44 -0400

    First commit

---
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if(product == null) return NotFound();
+
+            return Ok(product);
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        {
+            if(vo == null) return BadRequest();
+            var product = await _repository.Create(vo);
+            return CreatedAtAction(nameof(FindById), new { id =  product.Id }, product);
+        }
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        {
+            if (vo == null) return BadRequest();
+            var product = await _repository.Update(vo);
+            return CreatedAtAction(nameof(FindById), new { id = product.Id }, product);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if(!status) return BadRequest();
+
+            return Ok(status);
+        }
+
+
+    }
+}
---
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext() { }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Camisa mario kart",
+                Price = new decimal(69.00),
+                Description = "Description",
+                ImageUrl =  "Http://loremipson",
+                CategoryName = "T-shirt",
+            }); 
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Jogo Mario kart",
+                Price = new decimal(270.00),
+                Description = "Jogo",
+                ImageUrl =  "Http://loremipson",
+                CategoryName = "VideoGame",
+            });
+        }
+
+    }
+}
