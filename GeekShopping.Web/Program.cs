51c0921b7a33d3d9f2976980de430d80959a7757
Mario Camelo <mariuo@gmail.com>
2023-10-04 20:30:47 -0400

    Added Product Lists Page

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,8 +1,16 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//builder.Services.AddRazorPages();
 
+
+builder.Services.AddHttpClient<IProductService, ProductService>(
+    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductApi"])
+    );
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,8 +24,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+//app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+ 
 
 app.Run();
