d2928a6f5cf06392ef61a21ccf71bee0fd7cb803
Mario Camelo <mariuo@gmail.com>
2023-10-28 10:41:37 -0400

    OrderAPI created with migrations

---
+++ GeekShopping.OrderAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+
+        public DbSet<OrderDetail> Details { get; set; }
+        public DbSet<OrderHeader> Headers { get; set; }
+
+        
+    }
+}
