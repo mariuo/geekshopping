9bae4afe8c0824557103027edc14a10497fc489a
Mario Camelo <mariuo@gmail.com>
2023-11-05 05:57:37 -0500

    Changes MessageConsumer to Exchanges.Fanout

--- GeekShopping.PaymentAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
+++ GeekShopping.PaymentAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
@@ -64,7 +64,7 @@ namespace GeekShopping.PaymentAPI.MessageConsumer
 
             try
             {
-                _rabbitMQMessageSender.SendMessage(paymentResult, "orderpaymentresultqueue");
+                _rabbitMQMessageSender.SendMessage(paymentResult);
             }
             catch (Exception)
             {
